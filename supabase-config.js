4480bd6fb2d1b41b38243b97200584c481eb42e7
zhu626533147 <56953332+zhu626533147@users.noreply.github.com>
2025-12-30 11:05:12 +0800

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: 'https://pdhpwrftgkxmxplgpnvp.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkaHB3cmZ0Z2t4bXhwbGdwbnZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMzY5NTAsImV4cCI6MjA4MjYxMjk1MH0.yyu_FW5eHGM33-BcvmccaR83S5yHC4IVmRQhAx82_9g'
+};
